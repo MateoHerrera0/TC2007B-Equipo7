0f16bbd82dda4800b34f29102a615c01787f5e57
Akemi <akemi@Akemis-MacBook-Air.local>
2022-09-09 07:23:29 -0500

    Update navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return(
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="/">Alcaldía Álvaro obregón</a>
     <button className="navbar-toggler" type="button" onClick={() => { 
       if(expand === "collapse navbar-collapse") {
         setExpand("collapse navbar-collapse show")
@@ -19,13 +19,13 @@ export default function Navbar() {
     <div className={expand} id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="/home">Inicio</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="/newfile">Subir</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Buscar</a>
         </li>
       </ul>
     </div>
