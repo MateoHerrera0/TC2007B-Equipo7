6083f1ec0a84450dae873e403ac0f5cb5d08bee6
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2022-09-09 17:56:59 -0500

    Change fields

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,24 +13,24 @@ export default function Login() {
         event.preventDefault();
         }
         return (
-            <div class="container">
-                <div class="row">
-                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                    <div class="card border-0 shadow rounded-3 my-5">
-                    <div class="card-body p-4 p-sm-5">
-                        <h5 class="card-title text-center mb-5 fw-light fs-5">Ingresar</h5>
+            <div className="container">
+                <div className="row">
+                <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                    <div className="card border-0 shadow rounded-3 my-5">
+                    <div className="card-body p-4 p-sm-5">
+                        <h5 className="card-title text-center mb-5 fw-light fs-5">Ingresar</h5>
                         <form onSubmit={handleSubmit}>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="Usuario" />
-                            <label for="floatingInput">Usuario</label>
+                        <div className="form-floating mb-3">
+                            <label htmlFor="floatingInput">Usuario</label>
+                            <input type="email" className="form-control" id="floatingInput" placeholder="Usuario" />
                         </div>
-                        <div class="form-floating mb-3">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Constraseña" />
-                            <label for="floatingPassword">Constraseña</label>
+                        <div className="form-floating mb-3">
+                            <label htmlFor="floatingPassword">Constraseña</label>
+                            <input type="password" className="form-control" id="floatingPassword" placeholder="Constraseña" />
                         </div>
-                        <div class="d-grid">
+                        <div className="d-grid">
                             <Link to='/home'>
-                            <button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Entrar</button>
+                            <button className="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Entrar</button>
                         </Link>
                         </div>
                         </form>
