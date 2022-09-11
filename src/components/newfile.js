b4c4d86cfe2c54209777e63459f74c1b06753f8c
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2022-09-11 15:20:47 -0500

    Change field component

--- src/components/newfile.js
+++ src/components/newfile.js
@@ -1,7 +1,28 @@
-import { useState } from "react";
-import { fields } from "./fields"
+import { useState, useReducer } from "react";
+import { fields, Field } from "./fields"
+
+
+function reducer(state, event) {
+  if (event.reset) {
+    return {}
+  }
+  
+  return {...state,
+    [event.id]: event.value
+  }
+}
 
 export default function Newfile() {
+  const [formData, setFormData] = useReducer(reducer, {})
+  
+  function handleChange(ev) {
+    setFormData({
+      id: ev.target.name,
+      value: ev.target.value
+    })
+    console.log(formData);
+  }
+  
   const nulidadFields = [
     fields.expediente, 
     fields.tja,
@@ -14,6 +35,7 @@ export default function Newfile() {
     fields.demandado,
     fields.folio
   ]
+
   const carpetaFields = [
     fields.eco,
     fields.denuciante,
@@ -26,29 +48,38 @@ export default function Newfile() {
 
   const [fieldsToUse, setFields] = useState(nulidadFields)
   const usuario = 'getusuario'
+
   return(
      <div className="section p-5">
         <p className="text-center fs-1"><strong>Para subir tus documentos, ingresa los datos necesarios</strong></p>
         <p className="text-center fs-5">Selecciona un tipo de documento.</p>
         <div className="row text-center p-5">
           <div className="col">
-            <button type="button" className="btn btn-primary" onClick={() => setFields(nulidadFields)}>Juicio de Nulidad</button>
+            <button type="button" className="btn btn-primary" onClick={() => {setFields(nulidadFields); setFormData({reset: -1})}}>Juicio de Nulidad</button>
           </div>
           <div className="col">
-            <button type="button" className="btn btn-primary" onClick={() => setFields(carpetaFields)}>Carpeta de Investigacion</button>
+            <button type="button" className="btn btn-primary" onClick={() => {setFields(carpetaFields); setFormData({reset: -1})}}>Carpeta de Investigacion</button>
           </div>
         </div>
       <div className="container p-5 shadow rounded-3">
         <form>
-
           {fieldsToUse.map((value, index) => {
+            let id = value.id
             return(
-              value
+              <Field
+                field = {value}
+                onChange = {handleChange}
+                key = {index}
+                value = {formData[id] || ""}
+              />
             )
           })}
-          {fields.file}
-          <input type="hidden" id="usuario" value={usuario}/>
 
+          <div className="mb-3">
+            <label htmlFor="file" className="form-label">Documento Escaneado</label>
+            <input className="form-control" type="file" accept="*.pdf" id="file" />
+          </div>
+          <input type="hidden" name="usuario" id="usuario" value={usuario}/>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
