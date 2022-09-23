dfe290c809542377b1b8109335573311ac41ec51
karla-mond <karla.mondragon.rosas@gmail.com>
2022-09-22 20:08:12 -0500

    fetchs y busqueda

--- filesystemobregon/src/API/dbAPI.js
+++ filesystemobregon/src/API/dbAPI.js
@@ -1,5 +1,5 @@
 // Send the data to Mongo using an API
-export default async function addDocument(formData, setFormData, file) {
+async function addDocument(formData, setFormData, file) {
   // console.log("'addDocument' QUERY: " + JSON.stringify(formData));
   let formBody = new FormData(formData);
   // let formFile = new FormData();
@@ -28,19 +28,42 @@ export default async function addDocument(formData, setFormData, file) {
   }
 }
 
-async function getDocumentInfo(formData) {
-  let formBody = new formData(formData)
+async function getDocumentInfo(setData) {
   try {
     await fetch('api/getDocInfo', {
-      method: "GET",
-      body: formBody
+      method: "GET"
     })
-      .then(response => response.json())
-      .then(response => {
-        console.log("GetDocInfo Response: " + response);
-      })  
+      console.log("GetDocInfo Response: " + response); 
+      const data = await response.json();
+      console.log("Data received: " + {data});
+      setData(data);
   } catch (error) {
-    console.log("ERROR at 'addDocument'");
+    console.log("ERROR at 'getDocumentInfo'");
     console.log(error);
+    return [];
   } 
 } 
+
+async function getFilteredDoc(jQuery, setData) {
+  try {
+    await fetch('api/getFilteredDoc', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify(jQuery)
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log("getFilteredDoc response: " + response);
+        setData(response);
+      })
+  } catch (error) {
+    console.log("ERROR at 'getFilteredDoc'");
+    console.log(error);
+    return [];
+  } 
+}
+
+export { addDocument, getDocumentInfo, getFilteredDoc };
--- filesystemobregon/src/components/search.js
+++ filesystemobregon/src/components/search.js
@@ -1,8 +1,33 @@
 import React from "react";
 import './search.css';
-import { Link } from 'react-router-dom';
+import getDocumentInfo from "../API/dbAPI";
+import { useState } from "react";
 
-export default function Search() {
+const tagsNulidad = [
+    fields.expediente, 
+    fields.tja,
+    fields.actor,
+    fields.domicilio,
+    fields.acto,
+    fields.eJuridico,
+    fields.eProcesal,
+    fields.materia,
+    fields.demandado,
+    fields.folio
+]
+
+const tagsCarpeta = [
+    fields.eco,
+    fields.denuciante,
+    fields.imputado,
+    fields.delito,
+    fields.lugar,
+    fields.objeto,
+    fields.eGuarda,
+    fields.folio
+]
+
+function Search() {
     return(
         <div>
             
