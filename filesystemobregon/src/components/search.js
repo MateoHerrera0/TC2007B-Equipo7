7328a69c01b8a1de429aea7902ba4ed3835805ba
karla-mond <karla.mondragon.rosas@gmail.com>
2022-10-04 17:53:39 -0500

    sistema funcionando

--- filesystemobregon/src/components/search.js
+++ filesystemobregon/src/components/search.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import * as ReactDOM from "react-dom";
-import { Link } from 'react-router-dom';
+//import * as ReactDOM from "react-dom";
+//import { Link } from 'react-router-dom';
 import Navbar from './navbar'
 import { Grid, GridColumn, GridToolbar } from "@progress/kendo-react-grid";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
@@ -34,25 +34,65 @@ const Search = () => {
     setResult(process(data, event.dataState));
   }
 
+  const filterOperators = {
+    text: [
+        { text: 'grid.filterContainsOperator', operator: 'contains'},
+        { text: 'grid.filterNotContainsOperator', operator: 'doesnotcontain' },
+        { text: 'grid.filterEqOperator', operator: 'eq' },
+        { text: 'grid.filterNotEqOperator', operator: 'neq' },
+        { text: 'grid.filterStartsWithOperator', operator: 'startswith' },
+        { text: 'grid.filterEndsWithOperator', operator: 'endswith' },
+        // { text: 'grid.filterIsNullOperator', operator: 'isnull' },
+        // { text: 'grid.filterIsNotNullOperator', operator: 'isnotnull' },
+        // { text: 'grid.filterIsEmptyOperator', operator: 'isempty' },
+        // { text: 'grid.filterIsNotEmptyOperator', operator: 'isnotempty' }
+    ],
+    numeric: [
+        { text: 'grid.filterEqOperator', operator: 'eq' },
+        { text: 'grid.filterNotEqOperator', operator: 'neq' },
+        { text: 'grid.filterGteOperator', operator: 'gte' },
+        { text: 'grid.filterGtOperator', operator: 'gt' },
+        { text: 'grid.filterLteOperator', operator: 'lte' },
+        { text: 'grid.filterLtOperator', operator: 'lt' },
+        { text: 'grid.filterIsNullOperator', operator: 'isnull' },
+        { text: 'grid.filterIsNotNullOperator', operator: 'isnotnull' }
+    ],
+    date: [
+        { text: 'grid.filterEqOperator', operator: 'eq' },
+        { text: 'grid.filterNotEqOperator', operator: 'neq' },
+        { text: 'grid.filterAfterOrEqualOperator', operator: 'gte' },
+        { text: 'grid.filterAfterOperator', operator: 'gt' },
+        { text: 'grid.filterBeforeOperator', operator: 'lt' },
+        { text: 'grid.filterBeforeOrEqualOperator', operator: 'lte' },
+        { text: 'grid.filterIsNullOperator', operator: 'isnull' },
+        { text: 'grid.filterIsNotNullOperator', operator: 'isnotnull' }
+    ],
+    boolean: [
+      {text: "grid.filterEqOperator", operator: "eq"}
+    ],
+  };
+
+  //filterOperators={filterOperators};
+
   return(
     <div>
-      <br></br> <h2 id="Titulo"> Búsqueda </h2> <br></br>
+      <br></br> <h2 id="Titulo"> Búsqueda de Expedientes </h2> <br></br>
       <LocalizationProvider language="es-ES"> 
         <IntlProvider locale="es">
           <Grid
             data={res}
             style={{
-              height: "700px",
+              height: "auto",
             }}
             sortable={true}
-            //groupable={true}
+            groupable={true}
             filterable={true}
             pageable={{
               buttonCount: 4,
-              pageSizes: true,
+              pageSizes: true
             }}
             onDataStateChange={onDataStateChange}
-            filterOperators={esMessages}{...dataState}>
+            filterOperators={filterOperators}{...dataState}>
               <GridColumn field="expediente" title="Expediente" width="auto"/>
               <GridColumn field="tja" title="Sala del TJA" width="auto"/>
               <GridColumn field="actor" title="Actor" width="auto"/>
