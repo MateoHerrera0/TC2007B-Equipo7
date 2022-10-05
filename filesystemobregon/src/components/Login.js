6a6e628bde715d1ae0d09b9509125f96f8319eb0
karla-mond <karla.mondragon.rosas@gmail.com>
2022-10-04 19:44:37 -0500

    sistema de busqueda

--- filesystemobregon/src/components/Login.js
+++ filesystemobregon/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import './login.css'
+import './Login.css'
 import { Link } from 'react-router-dom';
 
 export default function Login(props) {
